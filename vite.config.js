f1989d84de347bbdea0236d64638d5b268f56308
ChocoFantasy <angel861107@gmail.com>
2025-01-02 19:53:28 +0800

    0102

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:process.env.NODE_ENV === 'production'?'/Mystic-Markers/':'/',
-
+  base: '/repo-name/', // Vite 設定
   plugins: [react()],
 })
